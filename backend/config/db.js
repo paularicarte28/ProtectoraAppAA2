3a4ea0a906f0d2b1e537ebd306e5988ab1db6cbd
paularicarte28 <a28729@svalero.com>
2025-06-19 00:07:31 +0200

    archivos generacion imagen docker

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306, // <- Añadido: puerto por defecto si no está en .env
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
